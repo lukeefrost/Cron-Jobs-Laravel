4a9ce713c9d15d0a3dd5a2c28751aaf4103502b3
lukelliotfrost <luke@radio.co>
2019-11-03 14:15:22 +0000

    Registered command on task scheduler

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        //
+        Commands\TestCron::class,
     ];
 
     /**
@@ -24,8 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')
-        //          ->hourly();
+        $schedule->command('demo:cron')->everyMinute();
     }
 
     /**
@@ -35,6 +34,8 @@ class Kernel extends ConsoleKernel
      */
     protected function commands()
     {
+        $this->load(__DIR__.'/Commands');
+        
         require base_path('routes/console.php');
     }
 }
